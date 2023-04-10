b45526540d1dc3300c87c8dbf256c9b227665405
Supakorn Kaewjaeng <70005081+nathsupakorn@users.noreply.github.com>
2023-04-10 21:59:37 +0700

    Add new Page

--- cloud-final-frontend/src/App.js
+++ cloud-final-frontend/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
+import GamePage from './pages/GamePage';
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <Switch>
-                    <Route path="/" element={<HomePage />}></Route>
-                </Switch>
+                <div className="App-header">
+                    <Switch>
+                        <Route path="/" element={<HomePage />}></Route>
+                        <Route path="/game" element={<GamePage />}></Route>
+                    </Switch>
+                </div>
             </div>
         </BrowserRouter>
     );
---
+++ cloud-final-frontend/src/pages/GamePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from 'antd';
+
+export default function GamePage() {
+
+    return (
+        <div>
+            <header>
+                <Button type="primary">CLICK ME</Button>
+            </header>
+        </div>
+    );
+}
